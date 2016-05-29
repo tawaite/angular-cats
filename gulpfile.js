cf06c5302234a29cc8f7790e2908ae1aa934e217
Tim Waite <twaite@credera.com>
2016-05-28 23:59:55 -0500

    Now serves an index file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,30 @@
 'use strict';
  
 var gulp = require('gulp'),
-    del = require('del');
+    del = require('del'),
+    connect = require('gulp-connect');
     
 gulp.task('clean', function() {
   return del['dist/*']
-})
+});
 
+gulp.task('html', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('serve', ['clean'], function() {
+  gulp.start('watch');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/**/*.html', ['html']);
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+});
+
+gulp.task('default', function() {
+  gulp.start('serve');
+});
